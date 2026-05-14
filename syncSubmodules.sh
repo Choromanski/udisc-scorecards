f7ac066259dfe80ca281ba50b53218e579eb1ac6
Brian Choromanski <BrianChoromanski@gmail.com>
2026-05-14 11:25:05 -0400

    Setup branch in submodule

--- syncSubmodules.sh
+++ syncSubmodules.sh
@@ -12,4 +12,11 @@ git submodule init
 git submodule status | while read -r sha path extras; do
     echo "Updating submodule: $path"
     git submodule update --init --remote "$path"
+
+    branch=$(git config -f .gitmodules "submodule.$path.branch")
+    branch=${branch:-master}
+
+    echo "Checking out $branch in $path"
+    git -C "$path" checkout "$branch"
+    git -C "$path" pull origin "$branch"
 done
